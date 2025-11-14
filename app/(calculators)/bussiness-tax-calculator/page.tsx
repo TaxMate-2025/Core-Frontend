a18496dd1f822bab7e33353a3bda98fc9e902153
Habib Adavize Abdulkareem <habibadavize94@gmail.com>
2025-11-14 23:43:39 +0100

    Update

---
+++ app/(calculators)/bussiness-tax-calculator/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import BusinessTaxCalculator from "@/components/BusinessTaxCalculator"
+
+export default function CalculatorPage() {
+  return (
+    <div>
+      <main className="flex-1 py-8 md:py-12">
+        <BusinessTaxCalculator />
+      </main>
+    </div>
+  )
+}
